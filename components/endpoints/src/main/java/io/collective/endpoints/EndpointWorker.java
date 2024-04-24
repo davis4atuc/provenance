f282033b93529fbcd5ea5dcf8a223e7e3f2c520b
Alex Davis <alex32davis@yahoo.com>
2024-04-24 14:52:07 -0600

    RSS XML mapping added to populate article data gateway with article items

--- components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
+++ components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
@@ -1,13 +1,17 @@
 package io.collective.endpoints;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.collective.articles.ArticleDataGateway;
 import io.collective.restsupport.RestTemplate;
+import io.collective.rss.Item;
+import io.collective.rss.RSS;
 import io.collective.workflow.Worker;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 
 public class EndpointWorker implements Worker<EndpointTask> {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -30,7 +34,12 @@ public class EndpointWorker implements Worker<EndpointTask> {
         String response = template.get(task.getEndpoint(), task.getAccept());
         gateway.clear();
 
-        { // todo - map rss results to an article infos collection and save articles infos to the article gateway
+        { // map rss results to an article infos collection and save articles infos to the article gateway
+            RSS rss = new XmlMapper().readValue(response, RSS.class);
+            List<Item> items = rss.getChannel().getItem();
+            for (Item item : items){
+                gateway.save( item.getTitle());
+            }
 
         }
     }
