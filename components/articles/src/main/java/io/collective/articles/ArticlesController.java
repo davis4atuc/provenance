dfcb68c63fa66613421cc1b390e6889e5bca7b6e
Alex Davis <alex32davis@yahoo.com>
2024-04-23 22:28:06 -0600

    Added retrieve code to get articles from articles gateway in ArticlesController

--- components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -6,6 +6,7 @@ import org.eclipse.jetty.server.Request;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticlesController extends BasicHandler {
@@ -20,15 +21,27 @@ public class ArticlesController extends BasicHandler {
     public void handle(String target, Request request, HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
 
-            { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
-
+            { // query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
+                List<ArticleRecord> allRecords = gateway.findAll();
+                List<ArticleInfo> articles = new ArrayList<>();
+                for (ArticleRecord record : allRecords ) {
+                    ArticleInfo article = new ArticleInfo(record.getId(), record.getTitle());
+                    articles.add(article);
+                }
+                writeJsonBody(servletResponse, articles);
             }
         });
 
         get("/available", List.of("application/json"), request, servletResponse, () -> {
 
-            { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
-
+            { // query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
+                List<ArticleRecord> allRecords = gateway.findAvailable();
+                List<ArticleInfo> articles = new ArrayList<>();
+                for (ArticleRecord record : allRecords ) {
+                    ArticleInfo article = new ArticleInfo(record.getId(), record.getTitle());
+                    articles.add(article);
+                }
+                writeJsonBody(servletResponse, articles);
             }
         });
     }
